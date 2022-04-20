5534522fbb59cc6bff2cc5b28cfc4aca1bb071df
AlexanderStadnyk <alexadnerstadnyk777@gmail.com>
2022-04-20 23:56:57 +0300

    third commit

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
-// npm - global command, comes with node
-// npm --version
+const http = require('http')
 
-// local dependency - use it only in particular project
-// npm i <packageName>
+const server = http.createServer((req,res)=>{
+    console.log('request event')
+    res.end('Hello world!')
+})
 
-// global dependency - use it in any project
-// npm install -g <packageName>
-// sudo npm install -g <packageName> (Mac)
-
-// package.json - manifest file (store important info about project/package):
-// - manual approach (create package.json in the root, create properties etc)
-// - npm init (step by step, press enter to skip)
-// - npm init -y (everything default)
-
-// why do we need package.json?
-// we need to provide an information about our project
-
-const _ = require('lodash')
-
-const items = [1, [2, [3, [4]]]]
-const newItems = _.flattenDeep(items)
-console.log(newItems)
+server.listen(3000, ()=>{
+    console.log('Server listening on port: 3000...')
+})
