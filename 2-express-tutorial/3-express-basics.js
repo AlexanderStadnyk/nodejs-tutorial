5aa114cb22070f489363f9fdb38c9e09ef7fda70
AlexanderStadnyk <alexadnerstadnyk777@gmail.com>
2022-05-17 22:44:34 +0300

    express

--- 2-express-tutorial/3-express-basics.js
+++ 2-express-tutorial/3-express-basics.js
@@ -3,11 +3,15 @@ const app = express()
 
 app.get('/', (req, res) => {
     console.log('user hit the resource')
-    res.send('Home Page')
+    res.status(200).send('Home Page')
 })
 
-app.get('/about', (req,res)=>{
-    res.send('About page')
+app.get('/about', (req, res) => {
+    res.status(200).send('About page')
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>resource not found</h1>')
 })
 
 app.listen(3000, () => {
